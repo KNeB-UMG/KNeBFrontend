4dc86150fb58d79916a505174b3d4b5222dd7ca7
Kacperros9 <Kacper.Gromala9@gmail.com>
2025-01-07 20:59:43 +0100

    finish

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Footer from '../components/Footer'
 
 export default function MainPage() {
   return (
     <div>
-      <h1>Mainpage</h1>
+      <Footer />
     </div>
   )
 }
