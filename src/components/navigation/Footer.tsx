4f02552f46167811e5664a5859a8cbf36cc48791
Kacperros9 <Kacper.Gromala9@gmail.com>
2025-01-10 20:15:02 +0100

    fixing footer

--- src/components/navigation/Footer.tsx
+++ src/components/navigation/Footer.tsx
@@ -1,74 +1,62 @@
-
-import { Col, Layout, Row } from 'antd';
+import { Col, Layout, Row, Flex, Typography, Space } from 'antd';
 import logo from '../../assets/logoKneb.png';
+
+const { Text } = Typography;
 
 export default function Footer() {
   return (
     <Layout.Footer
       style={{
-        bottom: 0,
-        left: 0,
-        padding: '20px 0',
+        padding: '2rem 10rem',
         boxShadow: '0px -4px 19.9px 0px #00000040',
       }}
     >
       <Row justify="space-between" gutter={[16, 16]} style={{alignItems: 'center'}}>
         <Col xs={24} sm={8} md={8} lg={8}>
-          <div
-            style={{
-              paddingLeft: '100px',
-              fontSize: '20px',
-              lineHeight: '24px',
-            }}
-          >
+          <Text>
+            <br />
             Wydział Zarządzania i Nauk o Jakości
             <br />
             ul. Morska 81-87 81-225
             <br />
             Gdynia tel. (58) 6901-215
-          </div>
+          </Text>
         </Col>
         <Col xs={24} sm={8} md={8} lg={8}>
-          <div
-            style={{
-              fontSize: '20px',
-              paddingLeft: '100px',
-            }}
-          >
-            Przewodniczący Kacper Kmiecik
+          <Text>
+            Przewodniczący Oskar Kacała
             <br />
             <a href="mailto:kneb@wznj.umg.edu.pl">kneb@wznj.umg.edu.pl</a>
-          </div>
+          </Text>
         </Col>
         <Col xs={24} sm={8} md={8} lg={8}>
-          <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+          <Flex justify='center' align='center'>
             <img
               src={logo}
-              alt="Koło Naukowe E-Biznesu Logo"
-              style={{width: '211.29px', marginRight: '10px'}}
+              alt="Logo KNeB"
+              style={{width: '7rem'}}
             />
-            <div>
-              <div
+            <Space direction="vertical">
+              <Text strong 
                 style={{
-                  fontSize: '29.2px',
-                  fontWeight: 'bold',
+                  fontSize: '1.5rem',
                   color: '#0071BC',
-                  lineHeight: '0',
+                  lineHeight: '1',
                 }}
               >
                 KOŁO NAUKOWE
-              </div>
-              <div
+              </Text>
+              <Text strong
                 style={{
-                  fontSize: '45.14px',
-                  fontWeight: 'bold',
+                  fontSize: '2.3rem',
                   color: '#000000',
+                  lineHeight: '0.1',
                 }}
               >
                 E-BIZNESU
-              </div>
-            </div>
-          </div>
+              </Text>
+            </Space>
+          </Flex>
         </Col>
       </Row>
     </Layout.Footer>
