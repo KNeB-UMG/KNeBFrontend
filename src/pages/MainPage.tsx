5403b658ca848883635d46f69cc2078963d59165
VitOR <wgorskiii@op.pl>
2025-01-26 16:46:31 +0100

    fix projectLinks

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,8 +1,10 @@
 import Management from "../components/main/Management";
+import ProjectLinks from "../components/main/ProjectLinks";
 
 export default function MainPage() {
   return (
     <div>
+      <ProjectLinks/>
       <Management/>
     </div>
   )
