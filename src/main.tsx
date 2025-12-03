cc851501d96678355109d9b8bccd67885483d59e
Kamil Niewiadomski <kamilniewiadomski18@gmail.com>
2025-12-03 14:00:56 +0100

    feat: delete existing website code (new website rework)

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,8 @@ import { createRoot } from 'react-dom/client'
 
 import App from './App.tsx'
 
-import ThemeProvider from './context/ThemeProvider.tsx'
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <App />
   </StrictMode>,
 )
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,21 +1,9 @@
 import React from 'react'
 
-import Header from '../components/home/Header'
-import Hero from '../components/home/Hero'
-import Projects from '../components/home/Projects'
-import Management from '../components/home/Management'
-import SocialMedia from '../components/home/SocialMedia'
-import Footer from '../components/reusable/Footer'
-
 const Home: React.FC = () => {
   return (
     <div className='home'>
-      <Header />
-      <Hero />
-      <Projects />
-      <Management />
-      <SocialMedia />
-      <Footer />
+      AAA
     </div>
   )
 }
